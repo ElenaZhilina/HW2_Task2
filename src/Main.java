f87fffaceddc687f167b66ee89820c5df698f208
Elena <elenazhilina@yandex.ru>
2024-02-20 14:52:35 +0600

    Java code added

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,15 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int x = 100;
+        int y = 300;
+
+        if (y >= 1000) {
+            int z = y / 100;
+            System.out.println("количество бонусных рублей = " + z);
+            System.out.println("итоговый счёт = " + (x + y + z));
+        } else {
+            System.out.println("количество бонусных рублей = 0");
+            System.out.println("итоговый счёт = " + (x + y));
+        }
     }
 }
