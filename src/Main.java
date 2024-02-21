739732340edf7ec8b2eb17f095a06bb61ed69d82
Elena <elenazhilina@yandex.ru>
2024-02-21 15:06:43 +0600

    condition changed

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
 public class Main {
     public static void main(String[] args) {
         int balance = 100;
-        int replenishment = 300;
+        int replenishment = 1000;
 
-        if (replenishment >= 1000) {
+        if (replenishment > 1000) {
             int bonus = replenishment / 100;
             System.out.println("количество бонусных рублей = " + bonus);
             System.out.println("итоговый счёт = " + (balance + replenishment + bonus));
